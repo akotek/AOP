d6bd422e7d383f5b10364984cc04779b668228b6
kotek <kotekmath@gmail.com>
2019-06-09 17:21:45 +0300

    ExceptionHandlerAspect impl, add logging to ExceptionTranslationAspect

--- src/aop/ExceptionHandlerAspect.java
+++ src/aop/ExceptionHandlerAspect.java
@@ -1,7 +1,29 @@
 package aop;
 
+import exceptions.KeyboardException;
+import org.aspectj.lang.JoinPoint;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import exceptions.UnhandledException;
 
 @Aspect
 public class ExceptionHandlerAspect {
+
+    private static Logger logger = LogManager.getLogger("exceptionLogger");
+
+    @Pointcut("execution(void SpaceWars.*(..))")
+    public void UnhandledExceptionScope() {}
+
+    @AfterThrowing(value="UnhandledExceptionScope()", throwing="t")
+    public void handleException(JoinPoint jp, KeyboardException t) {
+
+        // Intercept unhandled exceptions in application,
+        // Log it and throw generic UnhandledException(msg)
+
+        logger.error("Error in service {} with message {}", jp.getSignature(), t.getMessage());
+        throw new UnhandledException(t.getMessage());
+    }
 }
--- src/aop/ExceptionTranslationAspect.java
+++ src/aop/ExceptionTranslationAspect.java
@@ -2,12 +2,16 @@ package aop;
 
 import exceptions.EndGameException;
 import exceptions.KeyboardException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class ExceptionTranslationAspect {
+
+    private static Logger logger = LogManager.getLogger("exceptionLogger");
 
     @Around("execution(* SpaceWars.run())")
     public Object translateException(ProceedingJoinPoint pjp) throws Throwable {
@@ -18,6 +22,8 @@ public class ExceptionTranslationAspect {
         try {
             return pjp.proceed();
         } catch (KeyboardException e) {
+            logger.error("An {} exception has thrown, converting to application {} exception",
+                    e.getClass().getName(), EndGameException.class.getName());
             throw new EndGameException(e);
         }
     }
